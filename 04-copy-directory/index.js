e17f572c787c3c2e8e73cfbb517959fb6e426427
liakh83 <lahn4573@gmail.com>
2025-01-20 15:09:43 +0300

    feat: add solution for fourth task 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function copyFolder() {
+  const folderData = path.join(__dirname, 'files');
+  const copyFolderData = path.join(__dirname, 'files-copy');
+  try {
+    try {
+      await fs.rm(copyFolderData, { recursive: true }, { force: true });
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
+    }
+    await fs.mkdir(copyFolderData, { recursive: true });
+    await copyContent(folderData, copyFolderData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function copyContent(data, copyData) {
+  try {
+    const contents = await fs.readdir(data, { withFileTypes: true });
+
+    for (const content of contents) {
+      const dataPath = path.join(data, content.name);
+      const copyDataPath = path.join(copyData, content.name);
+      if (content.isDirectory()) {
+        await fs.mkdir(dataPath, { withFileTypes: true });
+        await copyContent(dataPath, copyDataPath);
+      } else if (content.isFile()) {
+        await fs.copyFile(dataPath, copyDataPath);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyFolder();
